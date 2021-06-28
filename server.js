b016023cddf5bd7227137fbbd266caf68d59150c
Graham <grahameganwyer@gmail.com>
2021-06-28 15:41:13 +1000

    grahams changes

--- server.js
+++ server.js
@@ -4,4 +4,17 @@ const port = 4567
 
 app.get('/', (req, res) => res.send('hello world'))
 
+console.log('Graham was here');
+
 app.listen(port, () => console.log(`listening on port: ${port}`))
+
+
+
+
+
+
+
+
+
+
+console.log('and also down here');
